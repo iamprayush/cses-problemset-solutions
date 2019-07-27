3a9b963efc5140a5bc131937609d0efeb33a000f
Prayush <iamprayush@gmail.com>
2019-07-27 14:45:45 +0530

    Added first solution

---
+++ weird-algorithm.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ 
+int main() {
+	long long int n;
+	cin >> n;
+	cout << n << " ";
+	while(n > 1) {
+		if(n&1) n = (3*n) + 1;
+		else	n >>= 1;
+		cout << n << " ";
+	}
+	return 0;
+}
