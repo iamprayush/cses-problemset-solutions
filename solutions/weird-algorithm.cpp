1fdc4f781a6292ad2570d9c2cd6e212970c1e531
Prayush <iamprayush@gmail.com>
2019-07-27 15:14:32 +0530

    Formatted Code

--- solutions/weird-algorithm.cpp
+++ solutions/weird-algorithm.cpp
@@ -1,13 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
- 
-int main() {
+
+int main()
+{
 	long long int n;
 	cin >> n;
 	cout << n << " ";
-	while(n > 1) {
-		if(n&1) n = (3*n) + 1;
-		else	n >>= 1;
+	while (n > 1)
+	{
+		if (n & 1)
+			n = (3 * n) + 1;
+		else
+			n >>= 1;
 		cout << n << " ";
 	}
 	return 0;
